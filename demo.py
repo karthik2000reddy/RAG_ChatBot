24220df76eaec7be2bea32cbd78219471ffb0c49
Karthik Alagoti <karthik.alagoti@letitbexai.com>
2025-09-18 15:37:28 +0530

    hi

---
+++ demo.py
@@ -0,0 +1,15 @@
+print("karthik")
+for i in range(100):
+    print("Hello World")
+for item in [1, 2, 3]:
+    print(item)
+if "apple" in ["apple", "banana", "cherry"]:
+    print("Yes, it's in the list")
+if "lo" in "Hello":
+    print("Substring found")
+if 5 not in [1, 2, 3, 4]:
+    print("5 is not in the list")
+if "karthik" in ["k","kk"]:
+    print("Yes, it's in the list")
+else :
+    print ("not, in the list ")
